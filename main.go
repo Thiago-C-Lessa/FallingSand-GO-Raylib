55502c88cb4ebbfcc6089ebc51aed0ed9f13f938
Thiago-C-Lessa <115082857+Thiago-C-Lessa@users.noreply.github.com>
2025-05-27 21:28:19 -0300

    send falling

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
-	
-	"FallingSand-GO-Raylib/sand_process"
-	"FallingSand-GO-Raylib/config"
-)
 
+	"FallingSand-GO-Raylib/config"
+	"FallingSand-GO-Raylib/sand_process"
+)
 
 func main() {
 	rl.InitWindow(config.SCREEN_WIDTH, config.SCREEN_HEIGHT, "Falling Sand")
@@ -17,7 +16,8 @@ func main() {
 	grid := sand_process.Intit() //y a esquerda e x a direita!!
 
 	for !rl.WindowShouldClose() {
-		
+
+		sand_process.GetInput(&grid)
 
 		sand_process.Update(&grid)
 
--- sand_process/sandPhysics.go
+++ sand_process/sandPhysics.go
@@ -6,7 +6,7 @@ import (
 	"FallingSand-GO-Raylib/config"
 )
 
-func Update(g *[][]int) {
+func GetInput(g *[][]int) {
 	if rl.IsMouseButtonDown(rl.MouseLeftButton) {
 		mouseX := int(rl.GetMouseX())
 		mouseY := int(rl.GetMouseY())
@@ -21,11 +21,39 @@ func Update(g *[][]int) {
 				ny := gridY + dy
 
 				// Verifica limites
-				if dx*dx+dy*dy <= config.MOUSE_RADIOUS * config.MOUSE_RADIOUS { //raio do circulo
+				if dx*dx+dy*dy <= config.MOUSE_RADIOUS*config.MOUSE_RADIOUS { //raio do circulo
 					if nx >= 0 && ny >= 0 && nx < config.COLS && ny < config.ROWS {
 						(*g)[ny][nx] = 1 //(*g) disrrefrencia po
 					}
 				}
+			}
+		}
+	}
+}
+
+func Update(g *[][]int) {
+	for i, _ := range *g {
+		for j, _ := range (*g)[i] {
+			if (*g)[i][j] == 1 {
+				moved := false
+
+				if i+1 < config.ROWS && (*g)[i+1][j] == 0 { //verifica bloco abaixo
+					(*g)[i+1][j] = 1
+					(*g)[i][j] = 0
+					moved = true
+				}
+
+				if !moved && i+1 < config.ROWS && j > 0 && (*g)[i+1][j-1] == 0 { //verifica a esquerda
+					(*g)[i+1][j-1] = 1
+					(*g)[i][j] = 0
+				}
+
+				if !moved && i+1 < config.ROWS && j+1 < config.COLS && (*g)[i+1][j+1] == 0 { //verifica a direita
+					(*g)[i+1][j+1] = 1
+					(*g)[i][j] = 0
+
+				}
+
 			}
 		}
 	}
